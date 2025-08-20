88d3d458148e6cf59c23fe1a9708080d9b8a3878
marilia <118003982+marilia22222@users.noreply.github.com>
2025-08-20 22:31:50 +0300

    Delete festival_with_base directory

--- festival_with_base/src/main/java/com/mycompany/festival/exception/FestivalNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.festival.exception;
-
-// Απλά κάν' την να επεκτείνει την RuntimeException. Αυτό είναι όλο!
-public class FestivalNotFoundException extends RuntimeException {
-    public FestivalNotFoundException(String message) {
-        super(message);
-    }
-}
